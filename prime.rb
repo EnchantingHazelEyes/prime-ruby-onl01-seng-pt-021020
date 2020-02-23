4a3529b0401138e626ccb7d3905a2723bee6b036
Samantha Barros <samantha.barros95@gmail.com>
2020-02-23 23:30:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,11 @@
-def prime(number)
-  
+def prime?(number)
+  start = 2
+  if number > 1
+    num_range = (start..number-1).to_a
+    num_range.none? do |num_to_test|
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
